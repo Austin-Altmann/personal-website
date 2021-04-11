575f3d3831524030d736f6787ef76b9087a3b09b
austin-altmann <38371172+Austin-Altmann@users.noreply.github.com>
2021-04-11 00:38:35 -0500

    Restyle home

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+function ShowAnnouncement() {
+    alert("hey every1!!! my mom took my AIM away so i cant talk right now :( ttyl lol :p");
+}
+
+function NavigateToPage(url) { 
+    window.location.href = url;
+} 
